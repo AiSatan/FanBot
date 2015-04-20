ccb5adfa45f0b62892b32da29f874fb73ccf3ce9
AiSatan <aisatan.ne@gmail.com>
2015-04-20 21:02:26 +0300

    добавлен  Awesomium

--- FantasyBot/Form1.Designer.cs
+++ FantasyBot/Form1.Designer.cs
@@ -29,11 +29,47 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.webControl1 = new Awesomium.Windows.Forms.WebControl(this.components);
+            this.webSessionProvider1 = new Awesomium.Windows.Forms.WebSessionProvider(this.components);
+            this.webControlContextMenu1 = new Awesomium.Windows.Forms.WebControlContextMenu(this.components);
+            this.SuspendLayout();
+            // 
+            // webControl1
+            // 
+            this.webControl1.ContextMenuStrip = this.webControlContextMenu1;
+            this.webControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webControl1.Location = new System.Drawing.Point(0, 0);
+            this.webControl1.Size = new System.Drawing.Size(381, 316);
+            this.webControl1.Source = new System.Uri("http://fantasyland.ru", System.UriKind.Absolute);
+            this.webControl1.TabIndex = 0;
+            // 
+            // webSessionProvider1
+            // 
+            this.webSessionProvider1.Views.Add(this.webControl1);
+            // 
+            // webControlContextMenu1
+            // 
+            this.webControlContextMenu1.Name = "webControlContextMenu1";
+            this.webControlContextMenu1.Size = new System.Drawing.Size(204, 126);
+            this.webControlContextMenu1.View = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 316);
+            this.Controls.Add(this.webControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Awesomium.Windows.Forms.WebControl webControl1;
+        private Awesomium.Windows.Forms.WebSessionProvider webSessionProvider1;
+        private Awesomium.Windows.Forms.WebControlContextMenu webControlContextMenu1;
     }
 }
 
